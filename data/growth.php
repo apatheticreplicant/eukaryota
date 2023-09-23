4a1e966e2e1f22d1c7244b17e1413e18a1967561
apatheticreplicant <burningcelery@protonmail.ch>
2023-09-23 13:43:50 -0600

    php

--- data/growth.php
+++ data/growth.php
@@ -1,6 +1,7 @@
 <?php
 
+header('Access-Control-Allow-Origin: *');
 $json = file_get_contents("php://input");
-$data = json_decode($son, true);
+$data = json_decode($json, true);
 
 var_dump($data);
