0d79e5dc7af8e7bef355aecaccb8df511558c364
apatheticreplicant <burningcelery@protonmail.ch>
2024-01-28 00:42:44 -0700

    Refresh directories

---
+++ posts/post.js
@@ -0,0 +1,21 @@
+// wordcount
+
+let article = document.querySelector("article").innerText
+let wordcount = article.split(" ").length;
+document.getElementById("wordcount").innerText = wordcount + ' words'
+
+console.log(wordcount)
+
+// accessdate 
+let currentdate = new Date();
+
+Date.prototype.today = function () { 
+    return ((this.getDate() < 10)?"0":"") + this.getDate() +"/"+(((this.getMonth()+1) < 10)?"0":"") + (this.getMonth()+1) +"/"+ this.getFullYear();
+}
+
+Date.prototype.timeNow = function () {
+     return ((this.getHours() < 10)?"0":"") + this.getHours() +":"+ ((this.getMinutes() < 10)?"0":"") + this.getMinutes() +":"+ ((this.getSeconds() < 10)?"0":"") + this.getSeconds();
+}
+
+let accessed = "This document was last accessed on " + new Date().today() + " at " + new Date().timeNow() + " (ATC)";
+document.getElementById("accessed").innerHTML = accessed
